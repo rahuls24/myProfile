be2a5a8eb9a9881a69fcbb3e54150a9c6e41274e
Rahul Kumar <rahul080924@outlook.com>
2019-12-17 22:41:18 +0530

    Updates

---
+++ precache-manifest.f0c42b711b32c1c8d62391c9dcf0e1c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6397bad7b4f9cf1681d26e3ec6fe0252",
+    "url": "/myProfile/index.html"
+  },
+  {
+    "revision": "631e1ed56abd8b855ef7",
+    "url": "/myProfile/static/css/main.56770d02.chunk.css"
+  },
+  {
+    "revision": "59d86d00b2b5413dd835",
+    "url": "/myProfile/static/js/2.af6d923d.chunk.js"
+  },
+  {
+    "revision": "1bf47b07947f2cc5b42ae6bc36f6bb19",
+    "url": "/myProfile/static/js/2.af6d923d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "631e1ed56abd8b855ef7",
+    "url": "/myProfile/static/js/main.bd7222ce.chunk.js"
+  },
+  {
+    "revision": "c40d526991845e49da49",
+    "url": "/myProfile/static/js/runtime-main.4608d5c6.js"
+  },
+  {
+    "revision": "7d8e8a32c2b3d49c546b914a29ac4cad",
+    "url": "/myProfile/static/media/dp.7d8e8a32.png"
+  }
+]);
